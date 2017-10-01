cad7c6c10d7ce4833211c8581003d05619790b8b
jhangpanerio <jhang.panerio@gmail.com>
2017-10-01 15:56:15 +0200

    Test commit

--- stuff/stuff/Program.cs
+++ stuff/stuff/Program.cs
@@ -10,6 +10,46 @@ namespace stuff
     {
         static void Main(string[] args)
         {
+
+         
         }
+
+        public static List<int> GenerateList()
+        {
+            List<int> stuff = new List<int>();
+
+      
+
+            return stuff;
+        }
+
+        public static void ForEachMethod2(List<int> l, int input1, int input2)
+        {
+          
+
+
+        }
+
+        public static void ForEachMethod()
+        {
+            Random random = new Random();
+
+      
+
+        }
+
+        public static void WhileTrueMethod(List<int> l, int input1, int input2)
+        {
+   
+
+
+
+        }
+
+
+
+
     }
+
 }
+
